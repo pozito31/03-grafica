b0d47318d46a03ead353369b942c7f3b37e647e8
pozito31 <jessicapozo198715@gmail.com>
2023-04-25 14:54:33 +0200

    subir proyecto a github

---
+++ src/app/components/grafica/grafica.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from '../../services/websocket.service';
+
+@Component({
+  selector: 'app-grafica',
+  templateUrl: './grafica.component.html',
+  styleUrls: ['./grafica.component.css'],
+})
+export class GraficaComponent implements OnInit {
+  public lineChartData: Array<any> = [{ data: [0, 0, 0, 0], label: 'Ventas' }];
+
+  public lineChartLabels: Array<any> = ['Enero', 'Febrero', 'Marzo', 'Abril'];
+
+  constructor(private http: HttpClient, public wsService: WebsocketService) {}
+
+  ngOnInit() {
+    // setInterval(() => {
+    //   const newData = [
+    //     Math.round(Math.random() * 100),
+    //     Math.round(Math.random() * 100),
+    //     Math.round(Math.random() * 100),
+    //     Math.round(Math.random() * 100),
+    //   ];
+    //   this.lineChartData = [{ data: newData, label: 'Ventas' }];
+    // }, 3000);
+    this.getData();
+    this.escucharSocket();
+  }
+
+  getData() {
+    this.http.get('http://localhost:5000/grafica').subscribe((data: any) => {
+      this.lineChartData = data;
+    });
+  }
+
+  escucharSocket() {
+    this.wsService.listen('cambio-grafica').subscribe((data: any) => {
+      console.log('socket', data);
+      this.lineChartData = data;
+    });
+  }
+}
